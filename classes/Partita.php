c4768193c16720e9d677bed9fe992687617df42b
Falli Matteo <lai2-24@itismeucci.com>
2024-03-26 13:49:12 +0100

    aggiunto attributo alla classe Partita

--- classes/Partita.php
+++ classes/Partita.php
@@ -5,13 +5,19 @@
         private $squadra2;
         private $goal1;
         private $goal2;
+        private $disputata;
 
-        function __construct($id, $squadra1, $squadra2, $goal1, $goal2){
+        function __construct($id, $squadra1, $squadra2, $goal1, $goal2, $disputata){
             $this->id = $id;
             $this->squadra1 = $squadra1;
             $this->squadra2 = $squadra2;
             $this->goal1 = $goal1;
             $this->goal2 = $goal2;
+            $this->disputata = $disputata;
+        }
+
+        function isDisputata(){
+            return $this->disputata;
         }
 
         function jsonSerialize(){
--- createObjects.php
+++ createObjects.php
@@ -25,23 +25,23 @@ function createTornei(){
         new Torneo(
             1,
             "Galluzzo",
-            "12/12/2023",
+            "10/12/2023",
             [
-                new Partita(1, $squadre[0], $squadre[1], 1, 2),
-                new Partita(2, $squadre[2], $squadre[3], 0, 3),
-                new Partita(3, $squadre[0], $squadre[2], 2, 0),
-                new Partita(4, $squadre[1], $squadre[3], 1, 1)
+                new Partita(1, $squadre[0], $squadre[1], 1, 2, true),
+                new Partita(2, $squadre[2], $squadre[3], 0, 3, true),
+                new Partita(3, $squadre[0], $squadre[2], 2, 0, true),
+                new Partita(4, $squadre[1], $squadre[3], 1, 1, true)
             ]
         ),
         new Torneo(
             2,
             "Impruneta",
-            "03/03/2025",
+            "21/03/2024",
             [
-                new Partita(1, $squadre[4], $squadre[5], 1, 2),
-                new Partita(2, $squadre[6], $squadre[7], 0, 3),
-                new Partita(3, $squadre[4], $squadre[6], 2, 0),
-                new Partita(4, $squadre[5], $squadre[7], 1, 1)
+                new Partita(1, $squadre[4], $squadre[5], 1, 2, true),
+                new Partita(2, $squadre[6], $squadre[7], 0, 3, true),
+                new Partita(3, $squadre[4], $squadre[6], 0, 0, false),
+                new Partita(4, $squadre[5], $squadre[7], 0, 0, false)
             ]
         )
     ];
