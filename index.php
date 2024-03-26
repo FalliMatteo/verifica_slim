eba5bec894c65bf33d61c32d6148fcea8affb122
Falli Matteo <lai2-24@itismeucci.com>
2024-03-26 12:51:09 +0100

    aggiornato index.php

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+include_once "controllers/IndexController.php";
+include_once "controllers/SquadreController.php";
+include_once "controllers/TorneoController.php";
+include_once "controllers/ApiController.php";
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/', "IndexController:index");
+$app->get('/torneo', "TorneoController:index");
+$app->get('/torneo/{codice}', "TorneoController:getTorneo");
+$app->get('/torneo/{codice}/partite', "TorneoController:getPartite");
+$app->get('/torneo/{codice}/partite/disputate', "TorneoController:getPartiteDisputate");
+$app->get('/torneo/{codice}/partite/da_disputare', "TorneoController:getPartiteDaDisputare");
+$app->get('/torneo/{codice}/partite/{id}', "TorneoController:getPartita");
+$app->get('/torneo/{codice}/concluso', "TorneoController:isConcluso");
+$app->get('/torneo/{codice}/classifica', "TorneoController:getClassifica");
+$app->get('/squadre', "SquadreController:index");
+$app->get('/squadre/{nome}', "SquadreController:getSquadra");
 
 $app->run();
